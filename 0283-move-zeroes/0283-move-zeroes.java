bf751efdd22ca00b7ba82dd26107f0c1cf42841b
manojkumarpr120920-commits <manojkumarpr120920@gmail.com>
2026-08-14 18:22:18 +0530

    Time: 2 ms (91.97%), Space: 46.9 MB (99.8%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,36 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+
+        int j = -1;
+
+        // Find the first zero
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] == 0) {
+                j = i;
+                break;
+            }
+        }
+
+        // No zero in the array
+        if (j == -1) {
+            return;
+        }
+
+        // Move non-zero elements to the front
+        for (int i = j + 1; i < nums.length; i++) {
+
+            if (nums[i] != 0) {
+
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+
+                j++;
+            }
+        }
+    }
+
+    // Time Complexity: O(n)
+    // Space Complexity: O(1)
+}
